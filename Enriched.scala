6b91bae280ce2cf23a20971b4104543c14c8be4a
raghu-kavali <60432331+raghu-kavali@users.noreply.github.com>
2020-04-22 19:33:22 -0400

    Add files via upload

--- Enriched.scala
+++ Enriched.scala
@@ -3,83 +3,68 @@ package ca.mcit.bigdata
 import org.apache.hadoop.fs.Path
 
 object Enriched extends Main with App {
-    val fileList = fs.listStatus(new Path("/user/fall2019"))
-    /*  val tripList = Source
-      .fromFile("/home/bd-user/Downloads/trips.txt")
-      .getLines()  */
-    val tripStream = fs.open(new Path("/user/fall2019/surya/stm/trips.txt"))
-    val tripList: List[Trips] = Iterator.continually(tripStream.readLine()).takeWhile(_ != null)
-      .toList
-      .tail
-      .map(_.split(",", -1))
-      .map(p => Trips(p(0).toInt, p(1), p(2), p(3), p(4).toInt, p(5).toInt, p(6).toInt,
-        if (p(7).isEmpty) None else Some(p(7)),
-        if (p(8).isEmpty) None else Some(p(8))))
+  val fileList = fs.listStatus(new Path("/user/fall2019"))
+  val tripStream = fs.open(new Path("/user/fall2019/nithin/stm/trips.txt"))
+  val tripList: List[Trips] = Iterator.continually(tripStream readLine()).takeWhile(_ != null)
+    .toList
+    .tail
+    .map(_.split(",", -1))
+    .map(p => Trips(p(0).toInt, p(1), p(2), p(3), p(4).toInt, p(5).toInt, p(6).toInt,
+      if (p(7).isEmpty) None else Some(p(7)),
+      if (p(8).isEmpty) None else Some(p(8))))
 
-    /* val routeList: List[Route] = Source
-      .fromFile("/home/bd-user/Downloads/routes.txt")
-      .getLines() */
-    val routeStream = fs.open(new Path("/user/fall2019/surya/stm/routes.txt"))
-    val routeList: List[Route] = Iterator.continually(routeStream.readLine()).takeWhile(_ != null)
-      .toList
-      .tail
-      .map(_.split(",", -1))
-      .map(r => Route(r(0).toInt, r(1), r(2), r(3), r(4), r(5), r(6), r(7)))
+  val routeStream = fs.open(new Path("/user/fall2019/nithin/stm/routes.txt"))
+  val routeList: List[Route] = Iterator.continually(routeStream readLine()).takeWhile(_ != null)
+    .toList
+    .tail
+    .map(_.split(",", -1))
+    .map(r => Route(r(0).toInt, r(1), r(2), r(3), r(4), r(5), r(6), r(7)))
 
-    val calendarStream = fs.open(new Path("/user/fall2019/surya/stm/calendar.txt"))
-    val calendar: List[Calendar] = Iterator.continually(calendarStream.readLine()).takeWhile(_ != null)
-      .toList
-      .tail
-      .map(_.split(",", -1))
-      .map(c => Calendar(c(0), c(1).toInt, c(2).toInt, c(3).toInt, c(4).toInt, c(5).toInt, c(6).toInt, c(7).toInt, c(8).toInt, c(9).toInt))
+  val calendarStream = fs.open(new Path("/user/fall2019/nithin/stm/calendar.txt"))
+  val calendar: List[Calendar] = Iterator.continually(elem = calendarStream.readLine()).takeWhile(_ != null)
+    .toList
+    .tail
+    .map(_.split(",", -1))
+    .map(c => Calendar(c(0), c(1).toInt, c(2).toInt, c(3).toInt, c(4).toInt, c(5).toInt, c(6).toInt, c(7).toInt, c(8).toInt, c(9).toInt))
 
+  val routeTrip: List[JoinOutput] = new GenericMapJoin[Trips, Route](L => L.route_id.toString)(R => R.route_id.toString)
+    .join(tripList, routeList)
 
-    val routeTrip: List[JoinOutput] = new GenericMapJoin[Trips, Route](L => L.route_id.toString)(R => R.route_id.toString)
-      .join(tripList, routeList)
+  val FinalEnriched: List[JoinOutput] = new GenericNestedLoopJoin[Calendar, JoinOutput]((i, j) =>
+    i.service_id == j.left.asInstanceOf[Trips].service_id)
+    .join(calendar, routeTrip)
 
+  val output: List[String] = FinalEnriched
+    .map(joinOutput => {
 
-    // routeTrip.foreach(println)
-    val FinalEnriched: List[JoinOutput] = new GenericNestedLoopJoin[Calendar, JoinOutput]((i, j) =>
-      i.service_id == j.left.asInstanceOf[Trips].service_id)
-      .join(calendar, routeTrip)
+      val x = Trips.toCsv(joinOutput.right.getOrElse("").asInstanceOf[JoinOutput].left.asInstanceOf[Trips])
+      val y = Route.toCsv(joinOutput.right.getOrElse("").asInstanceOf[JoinOutput].right.getOrElse("").asInstanceOf[Route])
+      val z = Calendar.toCsv(joinOutput.left.asInstanceOf[Calendar])
+      x + "," + y + "," + z
+    })
 
-    /* val HadoopEnrichedCsv: List[String] = HadoopEnriched
-      .map(JoinOutput => Trip.toCsv(JoinOutput.left.asInstanceOf[Trip]))
-  */
-    val output: List[String] = FinalEnriched
-      .map(joinOutput => {
+  val outputPath = new Path("/user/fall2019/nithin/course3/output.csv")
+  if (fs.exists(outputPath)) {
+    fs.delete(outputPath)
+    fs.mkdirs(new Path("/user/fall2019/nithin/course3"))
+    fs.createNewFile(new Path("/user/fall2019/nithin/course3/output.csv"))
+    val header = "service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date,route_id,service_id,trip_id,trip_headsign,direction_id,shape_id,wheelchair_accessible,note_fr,note_en, route_id,agency_id,route_short_name,route_long_name,route_type,route_url,route_color,route_text_color"
 
-        val x = Trips.toCsv(joinOutput.right.getOrElse("").asInstanceOf[JoinOutput].left.asInstanceOf[Trips])
-        val y = Route.toCsv(joinOutput.right.getOrElse("").asInstanceOf[JoinOutput].right.getOrElse("").asInstanceOf[Route])
-        val z = Calendar.toCsv(joinOutput.left.asInstanceOf[Calendar])
-        x + "," + y + "," + z
-      })
+    val outputStream = fs.append(outputPath)
+    outputStream.writeChars(header + "\n")
 
-    val outPath = new Path("/user/fall2019/surya/course3/Hadoop_Enriched.txt")
-    //fs.create(outPath, true )
-    if (fs.exists(outPath)) {
-      fs.delete(outPath)
-      fs.mkdirs(new Path("/user/fall2019/surya/course3"))
-      fs.createNewFile(new Path("/user/fall2019/surya/course3/Hadoop_Enriched.csv"))
-      val header = "service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date,route_id,service_id,trip_id,trip_headsign,direction_id,shape_id,wheelchair_accessible,note_fr,note_en, route_id,agency_id,route_short_name,route_long_name,route_type,route_url,route_color,route_text_color"
-
-      val outputStream = fs.append(outPath)
-      outputStream.writeChars(header + "\n")
-
-      output.foreach(FinalEnriched => outputStream.writeChars(FinalEnriched + "\n"))
-      outputStream.close()
-
-    }
-
-    else {
-      fs.createNewFile(new Path("/user/fall2019/surya/course3/Hadoop_Enriched.txt"))
-      val header = "service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date,route_id,service_id,trip_id,trip_headsign,direction_id,shape_id,wheelchair_accessible,note_fr,note_en, route_id,agency_id,route_short_name,route_long_name,route_type,route_url,route_color,route_text_color"
-
-      val outputStream = fs.append(outPath)
-      outputStream.writeChars(header + "\n")
-      output.foreach(FinalEnriched => outputStream.writeChars(FinalEnriched + "\n"))
-      outputStream.close()
-
-    }
-    fs.close()
+    output.foreach(FinalEnriched => outputStream.writeChars(FinalEnriched + "\n"))
+    outputStream.close()
   }
+  else {
+    fs.createNewFile(new Path("/user/fall2019/nithin/course3/output.csv"))
+    val header = "service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date,route_id,service_id,trip_id,trip_headsign,direction_id,shape_id,wheelchair_accessible,note_fr,note_en, route_id,agency_id,route_short_name,route_long_name,route_type,route_url,route_color,route_text_color"
+
+    val outputStream = fs.append(outputPath)
+    outputStream.writeChars(header + "\n")
+    output.foreach(FinalEnriched => outputStream.writeChars(FinalEnriched + "\n"))
+    outputStream.close()
+  }
+  fs.close()
+}
+
---
+++ Route.scala
@@ -0,0 +1,24 @@
+package ca.mcit.bigdata
+
+case class Route( route_id: Int,
+                  agency_id: String,
+                  route_short_name: String,
+                  route_long_name: String,
+                  route_type: String,
+                  route_url: String,
+                  route_color: String,
+                  route_text_color: String
+                )
+
+object Route {
+  def toCsv(route: Route): String = {
+    route.route_id + "," +
+      route.agency_id + "," +
+      route.route_short_name + "," +
+      route.route_long_name + "," +
+      route.route_type + "," +
+      route.route_url + "," +
+      route.route_color + "," +
+      route.route_text_color
+  }
+}
